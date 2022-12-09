5a7a0223ea3f0e69910a1ad87cd774e364ba8d3e
Mates0 <matyas.vanko@educanet.cz>
2022-12-09 09:54:05 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Data from './data.json';
 import {useState} from 'react';
 import './style.css';
 export default function App() {
+    const [favouriteAirport,setfavouriteAirports] = useState([])
   return (
       <div className="outer-box">
         <h1>Oblíbená letiště</h1>
